0de0146ed417ea3b50d0a335034586afbee178c2
SAINIKHILK5 <komuravellisainikhil02@gmail.com>
2024-03-21 10:48:22 +0000

    firstCommit

---
+++ src/main/java/com/example/song/repository/SongRepository.java
@@ -0,0 +1,12 @@
+package com.example.song.repository;
+
+//package com.example.song.repository;
+
+//package com.example.song.repository.*;
+
+import java.util.ArrayList;
+import com.example.song.model.*;
+
+public interface SongRepository {
+    public ArrayList<Song> getSongs();
+}
